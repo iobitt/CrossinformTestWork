eb712cefc5cc58584459c30ba386d9e12099a2b9
iobitt <sashabratm@gmail.com>
2021-03-23 18:59:49 +0300

    Добавьте файлы проекта.

---
+++ CrossinformTestWork/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics; // подключаем пространство имён, в котором находится класс Stopwatch
+using System.Threading;
+
+
+namespace CrossinformTestWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch time = new Stopwatch();
+
+            time.Start();
+            
+            time.Stop();
+            Console.WriteLine("Время работы программы:");
+            Console.WriteLine(time.Elapsed);
+
+            Console.ReadKey();
+        }
+    }
+}
